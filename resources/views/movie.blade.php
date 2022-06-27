a216716f568a1536eeba7dcac17d9831a48afd2a
R3n1t0 <98884076+R3n1t0@users.noreply.github.com>
2022-06-27 11:52:27 +0200

    stampo nella pagina movie

--- resources/views/movie.blade.php
+++ resources/views/movie.blade.php
@@ -4,7 +4,27 @@
 @section("content")
 
     <div class="container">
-        <h1>Lista Film</h1>
+        <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">#ID</th>
+                <th scope="col">Titolo</th>
+                <th scope="col">Data di uscita</th>
+                <th scope="col">Voto</th>
+              </tr>
+            </thead>
+            <tbody>
+                @foreach ($movies as $movie)
+                    <tr>
+                        <th scope="row">{{ $movie->id }}</th>
+                        <td>{{ $movie->title }}</td>
+                        <td>{{ $movie->date }}</td>
+                        <td>{{ $movie->vote }}</td>
+                    </tr>
+                @endforeach
+
+            </tbody>
+          </table>
     </div>
 
 @endsection
